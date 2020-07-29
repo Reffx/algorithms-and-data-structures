31ec5a65e7162f4ee48598e1731e04dcf0b7f39d
reffx <ripsli@hotmail.com>
2020-07-29 18:30:34 +0200

    bubble working

--- sort_algorithms/bubble.go
+++ sort_algorithms/bubble.go
@@ -1,20 +1,36 @@
+package main
+
 import "fmt"
 
 func main() {
-	var numbers []int = []int{5,4,3,2,1,0} 
+	var numbers []int = []int{5, 4, 3, 2, 1, 0}
 	fmt.Println("Our list of numbers is:", numbers)
-	sweep(numbers)
+	bubbleSort(numbers)
+	fmt.Println("After sorting:", numbers)
 }
 
-func sweep(numbers []int){
+func bubbleSort(numbers []int) {
 	var N int = len(numbers)
-	var firstIndex int = 0;
+	var i int
+	for i = 0; i < N; i++ {
+		sweep(numbers)
+		fmt.Println("Sweep Nr", i+1, numbers)
+	}
+}
+
+func sweep(numbers []int) {
+	var N int = len(numbers)
+	var firstIndex int = 0
 	var secondIndex int = 1
 
 	for secondIndex < N {
 		var firstNumber int = numbers[firstIndex]
 		var secondNumber int = numbers[secondIndex]
-		fmt.Println("Comparing the following:", firstNumber, secondNumber)
+		if firstNumber > secondNumber {
+			//Swap the numbers
+			numbers[firstIndex] = secondNumber
+			numbers[secondIndex] = firstNumber
+		}
 
 		firstIndex++
 		secondIndex++
