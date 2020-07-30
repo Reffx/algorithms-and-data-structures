5eb0872c8f3202a3b40b9e5634ddf399baacb40b
reffx <ripsli@hotmail.com>
2020-07-30 14:34:18 +0200

    Bubble sort improved version added

--- sort_algorithms/bubble.go
+++ sort_algorithms/bubble.go
@@ -3,10 +3,16 @@ package main
 import "fmt"
 
 func main() {
+	//Simple bubble sort
 	var numbers []int = []int{5, 4, 3, 2, 1, 0}
 	fmt.Println("Our list of numbers is:", numbers)
 	bubbleSort(numbers)
 	fmt.Println("After sorting:", numbers)
+	//Improved bubble sort
+	var numbersImproved []int = []int{1, 2, 3, 2, 4, 5}
+	fmt.Println("(Improved) Our list of numbers is:", numbersImproved)
+	bubbleSortImproved(numbersImproved)
+	fmt.Println("After sorting:", numbersImproved)
 }
 
 func bubbleSort(numbers []int) {
@@ -35,4 +41,41 @@ func sweep(numbers []int) {
 		firstIndex++
 		secondIndex++
 	}
+}
+
+// Improvements
+// 1. At the first sweep the highest number is already at the end, so we don't need to check the full array anymore
+// 2. Check if the list is already sorted
+func bubbleSortImproved(numbers []int) {
+	var N int = len(numbers)
+	var i int
+	for i = 0; i < N; i++ {
+		if !sweepImproved(numbers, i) { // I2 if statement
+			return
+		}
+		fmt.Println("Sweep Nr", i+1, numbers)
+	}
+}
+
+// return boolean true is a value was swapped
+func sweepImproved(numbers []int, prevPasses int) bool { // I1,I2
+	var N int = len(numbers)
+	var firstIndex int = 0
+	var secondIndex int = 1
+	var didSwap bool = false // I2
+
+	for secondIndex < (N - prevPasses) { // I1
+		var firstNumber int = numbers[firstIndex]
+		var secondNumber int = numbers[secondIndex]
+		if firstNumber > secondNumber {
+			//Swap the numbers
+			numbers[firstIndex] = secondNumber
+			numbers[secondIndex] = firstNumber
+			didSwap = true // I2
+		}
+
+		firstIndex++
+		secondIndex++
+	}
+	return didSwap //I2
 }
